8ff004e8bbb935547a8192411e75474944d0eb70
Niyazi Emre Akbaş <emreakbas46@outlook.com>
2024-05-11 02:30:25 +0300

    new stealable objects defined

---
+++ Assets/Objects/StealableObjects/StealableObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StealableObject : MonoBehaviour
+{
+    //public new string name;
+
+    public int scorePoint;
+
+    public string objectClass;
+
+    //public StealableObjectType type;
+
+    private void Start()
+    {
+        //this.GetComponent<SpriteRenderer>().sprite = type.sprite;
+    }
+
+
+}
