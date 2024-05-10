8d5df2bce201296510804ff6191d64f8326a4167
Fatih <faatihboz@gmail.com>
2024-05-10 23:18:13 +0300

    initialize NavMesh

--- Assets/NavMeshAgentController.cs
+++ Assets/NavMeshAgentController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshAgentController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform target;
+
+
+    private NavMeshAgent agent;
+
+
+    private void Start()
     {
-        
+
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        agent.SetDestination(target.position);
     }
 }
