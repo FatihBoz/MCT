6d2ba35907fb51a54cb9f0b26329fbb760c82aa9
Niyazi Emre Akbaş <emreakbas46@outlook.com>
2024-05-11 05:08:44 +0300

    stealable

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -1,33 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed=5f;
+    public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Animator anim;
     private Vector2 movement;
+
+    public TextMeshProUGUI text;
+    public bool stealable;
     void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
-        anim=GetComponent<Animator>();
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
     }
 
     void Update()
     {
-        movement.x=Input.GetAxisRaw("Horizontal");
-        movement.y=Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
         movement.Normalize();
         AnimationsSet();
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            stealable = true;
+            Debug.Log("Toplanabilir");
+
+        }
     }
-    private void FixedUpdate() {
-           rb.MovePosition(rb.position+moveSpeed * Time.fixedDeltaTime * movement);
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * movement);
     }
-    private void AnimationsSet(){
-        anim.SetFloat("velocity_x",movement.x);
-        anim.SetFloat("velocity_y",movement.y);
-        anim.SetBool("isMoving",movement!=Vector2.zero);
+    private void AnimationsSet()
+    {
+        anim.SetFloat("velocity_x", movement.x);
+        anim.SetFloat("velocity_y", movement.y);
+        anim.SetBool("isMoving", movement != Vector2.zero);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Stealable"))
+        {
+            text.gameObject.SetActive(true);
+            if (stealable)
+            {
+                if (collision.gameObject.CompareTag("Stealable"))
+                {
+                    Destroy(collision.gameObject);
+                    text.gameObject.SetActive(false);
+
+                }
+
+            }
+        }
+
     }
 }
