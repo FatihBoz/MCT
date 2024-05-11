9cb058abff9cad04bfae99521afe79eec09af492
Fatih <faatihboz@gmail.com>
2024-05-11 21:56:11 +0300

    Checklist tamamlandı

--- Assets/CheckList-UI/Scripts/CheckList.cs
+++ Assets/CheckList-UI/Scripts/CheckList.cs
@@ -6,13 +6,39 @@ using DG.Tweening;
 
 public class CheckList : MonoBehaviour
 {
+    #region PUBLIC
     public int minLine;
     public int maxLine;
-
     public float fadeDuration;
+
+    public Vector3 publicOffSet;
+    public Vector3 privateOffSet;
+    #endregion
 
     [SerializeField] private List<TextMeshProUGUI> lines;
     [SerializeField] private List<StealableObject> objects;
+
+    #region PRIVATE
+    private CheckListObject[] checkListObjects;
+
+    private int lineCount;
+    private bool isOpen;
+    private Vector3 publicPos;
+    private Vector3 privatePos;
+
+    private RectTransform rectTransform;
+    #endregion
+
+    private void OnEnable()
+    {
+        PlayerMovement.OnObjectStolen += CheckList_OnObjectStolen;
+    }
+
+    private void Awake()
+    {
+        SetOffSet();
+        checkListObjects = new CheckListObject[objects.Count];
+    }
 
     private void Start()
     {
@@ -21,7 +47,9 @@ public class CheckList : MonoBehaviour
             return;
         }
 
-        int lineCount = Random.Range(minLine,maxLine+1);
+        SelectObjectToSteal();
+
+        lineCount = Random.Range(minLine,maxLine+1);
 
         MakeLinesVisible(lineCount);
 
@@ -41,11 +69,61 @@ public class CheckList : MonoBehaviour
         int c = 0;
         while (c < lines.Count)
         {
-
             int sel = Random.Range(0,objects.Count);
 
-            
+            CheckListObject obj = new(objects[sel].name, Random.Range(1,objects[sel].maxCount+1), lines[c]);
+            checkListObjects[c] = obj;
+
+            objects.RemoveAt(sel);
+
+            c++;
         }
     }
 
+
+    private void CheckList_OnObjectStolen(StealableObject obj)
+    {
+        for(int i = 0;i < lineCount;i++)
+        {
+            if (obj.name.Equals(checkListObjects[i].GetName()))
+            {
+                checkListObjects[i].ObjectHasStolen();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if(isOpen)
+            {
+                rectTransform.DOMove(privatePos, 0.75f);
+                isOpen = false;
+            }
+            else
+            {
+                rectTransform.DOMove(publicPos, 0.75f);
+                isOpen= true;
+            }
+        }
+    }
+
+    void SetOffSet()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        privatePos = rectTransform.position + privateOffSet;
+        publicPos = rectTransform.position + publicOffSet;
+
+        rectTransform.position = privatePos;
+    }
+
+
+    private void OnDisable()
+    {
+        PlayerMovement.OnObjectStolen += CheckList_OnObjectStolen;
+    }
+
+ 
 }
