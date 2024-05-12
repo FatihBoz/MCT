c69be2513e63441722f6b80b3c5476d96a88de2e
Niyazi Emre Akbaş <emreakbas46@outlook.com>
2024-05-12 12:32:07 +0300

    tutorial texts added

---
+++ Assets/GUI-UI/Tutorial/Tutorial.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Tutorial")]
+public class Tutorial : ScriptableObject
+{
+    private int sequanceNumber;
+    public String text;
+
+
+}
